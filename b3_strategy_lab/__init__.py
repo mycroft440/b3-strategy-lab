d5044fa8371fce9514a6ce00ae29bdf0602ed878
mycroft440 <alquimistadodetalhe@gmail.com>
2026-09-04 07:20:00 -0300

    Install supplemental registry scope patch

--- b3_strategy_lab/__init__.py
+++ b3_strategy_lab/__init__.py
@@ -1,5 +1,6 @@
 from .candles import DEFAULT_TICKERS
 from . import causal_liquidity as _causal_liquidity  # installs fail-closed realistic execution patch
 from . import audit_hardening as _audit_hardening  # installs audited accounting corrections
+from . import supplemental_scope_patch as _supplemental_scope_patch  # scopes global supplemental registry to the selected universe
 
 __all__ = ["DEFAULT_TICKERS"]
